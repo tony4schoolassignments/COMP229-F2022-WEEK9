c853277feab13040b537532053ae937af9786d12
Junesik (Tony) Han <jhan80@my.centennialcollege.ca>
2022-11-14 13:23:56 -0500

    week9

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './content/about/about.component';
+import { LoginComponent } from './content/auth/login/login.component';
+import { ContactComponent } from './content/contact/contact.component';
+import { HomeComponent } from './content/home/home.component';
+import { ProjectsComponent } from './content/projects/projects.component';
+import { ServicesComponent } from './content/services/services.component';
+
+const routes: Routes = [
+  {path: '', redirectTo:'home', pathMatch:'full'},
+  {path: 'home', component: HomeComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'projects', component: ProjectsComponent},
+  {path: 'services', component: ServicesComponent},
+  {path: 'contact', component: ContactComponent},
+  {path: 'login', component: LoginComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://comp229-f2022-401.herokuapp.com/api'
+};
